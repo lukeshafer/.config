60ed4a3955d8e327453e2f052628626b71ccdf11
Luke <hello@lukeshafer.com>
2023-02-06 00:58:04 -0500

    update personal .zshrc

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -35,7 +35,6 @@ require("null-ls").setup({
 				group = augroup,
 				buffer = bufnr,
 				callback = function()
-					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
 					vim.lsp.buf.format({ bufnr = bufnr })
 				end,
 			})
