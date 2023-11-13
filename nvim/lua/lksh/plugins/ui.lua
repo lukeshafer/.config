996e701ff9b8521ee3952ad88f153ed1570d28fd
Luke Shafer <lukealanshafer@gmail.com>
2023-11-13 16:49:49 -0500

    Remove notifier

--- nvim/lua/lksh/plugins/ui.lua
+++ nvim/lua/lksh/plugins/ui.lua
@@ -90,11 +90,11 @@ return function()
 		{
 			'rcarriga/nvim-notify',
 			config = function()
-				local notify = require('notify')
+				--local notify = require('notify')
 
-				vim.notify = function(message, level, opts)
-					return notify(message, level, opts) -- <-- Important to return the value from `nvim-notify`
-				end
+				--vim.notify = function(message, level, opts)
+					--return notify(message, level, opts) -- <-- Important to return the value from `nvim-notify`
+				--end
 			end
 		},
 
