92d5932f559ba9e6e7dbb52efc23c31ef7130d73
Luke <lukealanshafer@gmail.com>
2024-04-25 21:43:15 -0400

    Updates

--- nvim/lua/lksh/set.lua
+++ nvim/lua/lksh/set.lua
@@ -32,14 +32,14 @@ local foldclose = fcs.foldclose or "+"
 
 -- Stolen from Akinsho
 local function get_fold(lnum)
-	if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
-		return " "
-	end
+  if vim.fn.foldlevel(lnum) <= vim.fn.foldlevel(lnum - 1) then
+    return " "
+  end
   return vim.fn.foldclosed(lnum) == -1 and foldopen or foldclose
 end
 
 _G.get_statuscol = function()
-	return "%s%l " .. get_fold(vim.v.lnum) .. " "
+  return "%s%l " .. get_fold(vim.v.lnum) .. " "
 end
 
 vim.o.statuscolumn = "%!v:lua.get_statuscol()"
