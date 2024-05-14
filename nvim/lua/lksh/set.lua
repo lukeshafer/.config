62fcc0979ed30c85df6fb28c92e6b7a775e9e6e4
Luke Shafer <lukealanshafer@gmail.com>
2024-05-14 10:21:53 -0400

    Update for windows usage

--- nvim/lua/lksh/set.lua
+++ nvim/lua/lksh/set.lua
@@ -1,4 +1,9 @@
+local IS_WINDOWS = vim.loop.os_uname().sysname == 'Windows_NT'
 HOME = os.getenv("HOME")
+if not HOME then
+  HOME = os.getenv("USERPROFILE")
+  IS_WINDOWS = true
+end
 
 -- Required for NvimTree
 vim.g.loaded_netrw = 1
@@ -12,8 +17,13 @@ vim.opt.expandtab = true
 vim.opt.background = "dark" -- sets light/dark for some colorschemes
 vim.opt.number = true
 vim.opt.numberwidth = 2
-vim.opt.backupdir = HOME .. "/.vim/backup"
-vim.opt.dir = HOME .. "/.vim/swapfiles"
+if IS_WINDOWS then
+  vim.opt.backupdir = HOME .. "\\.vim\\backup"
+  vim.opt.dir = HOME .. "\\.vim\\swapfiles"
+else
+  vim.opt.backupdir = HOME .. "/.vim/backup"
+  vim.opt.dir = HOME .. "/.vim/swapfiles"
+end
 vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
 vim.opt.mouse = "a"
 vim.opt.cursorline = true
