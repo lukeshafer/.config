cc9e6b12f0c134baa827314527197c02808d3475
Luke Shafer <lukealanshafer@gmail.com>
2025-02-27 22:42:57 -0500

    Add zen mode

--- nvim/lua/lksh/plugins.lua
+++ nvim/lua/lksh/plugins.lua
@@ -742,6 +742,19 @@ local plugins = {
 			"RainbowMultiDelim",
 		},
 	},
+	-- Lua
+	{
+		"folke/zen-mode.nvim",
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+		keys = {
+			{ "<leader>z", "<cmd>ZenMode<cr>" },
+		},
+		cmd = { "ZenMode" },
+	},
 }
 
 require("lazy").setup(plugins)
