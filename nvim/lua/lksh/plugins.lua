46b9d8b072152359ea9ffd9a8a8e581881855459
Luke Shafer <shafer.361@osu.edu>
2025-03-28 16:48:01 -0400

    Statusline

--- nvim/lua/lksh/plugins.lua
+++ nvim/lua/lksh/plugins.lua
@@ -346,58 +346,58 @@ local plugins = {
 		end,
 	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true },
-	{
-		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-		opts = {
-			sections = {
-				lualine_a = {
-					{
-						"mode",
-						fmt = function(str)
-							if str == "NORMAL" then
-								return ""
-							elseif str == "INSERT" then
-								return ""
-							elseif str == "VISUAL" or str == "V-LINE" or str == "V-BLOCK" then
-								return "󰗧"
-							elseif str == "COMMAND" then
-								return ""
-							else
-								return str
-							end
-						end,
-					},
-				},
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{
-						"filename",
-						symbols = { modified = "●", readonly = "[READONLY]" },
-						path = 4,
-					},
-				},
-				lualine_x = { "encoding", "filetype" },
-				lualine_y = {},
-				lualine_z = { "location" },
-			},
-			inactive_sections = {
-				lualine_a = {},
-				lualine_b = {},
-				lualine_c = {
-					{
-						"filename",
-						symbols = { modified = "●", readonly = "[READONLY]" },
-						path = 4,
-					},
-				},
-				lualine_x = {},
-				lualine_y = {},
-				lualine_z = {},
-			},
-			extensions = { "nvim-tree", "toggleterm", "man" },
-		},
-	}, -- status line, bottom
+	-- {
+	-- 	"nvim-lualine/lualine.nvim",
+	-- 	dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	-- 	opts = {
+	-- 		sections = {
+	-- 			lualine_a = {
+	-- 				{
+	-- 					"mode",
+	-- 					fmt = function(str)
+	-- 						if str == "NORMAL" then
+	-- 							return ""
+	-- 						elseif str == "INSERT" then
+	-- 							return ""
+	-- 						elseif str == "VISUAL" or str == "V-LINE" or str == "V-BLOCK" then
+	-- 							return "󰗧"
+	-- 						elseif str == "COMMAND" then
+	-- 							return ""
+	-- 						else
+	-- 							return str
+	-- 						end
+	-- 					end,
+	-- 				},
+	-- 			},
+	-- 			lualine_b = { "branch", "diff", "diagnostics" },
+	-- 			lualine_c = {
+	-- 				{
+	-- 					"filename",
+	-- 					symbols = { modified = "●", readonly = "[READONLY]" },
+	-- 					path = 4,
+	-- 				},
+	-- 			},
+	-- 			lualine_x = { "encoding", "filetype" },
+	-- 			lualine_y = {},
+	-- 			lualine_z = { "location" },
+	-- 		},
+	-- 		inactive_sections = {
+	-- 			lualine_a = {},
+	-- 			lualine_b = {},
+	-- 			lualine_c = {
+	-- 				{
+	-- 					"filename",
+	-- 					symbols = { modified = "●", readonly = "[READONLY]" },
+	-- 					path = 4,
+	-- 				},
+	-- 			},
+	-- 			lualine_x = {},
+	-- 			lualine_y = {},
+	-- 			lualine_z = {},
+	-- 		},
+	-- 		extensions = { "nvim-tree", "toggleterm", "man" },
+	-- 	},
+	-- }, -- status line, bottom
 	{ "lewis6991/gitsigns.nvim", opts = {} }, -- inline git helpers
 	{ "folke/todo-comments.nvim", dependencies = "nvim-lua/plenary.nvim", opts = {} }, -- highlight todo comments, e.g. TODO:
 	{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} }, -- auto-create brackets/parentheses
--- nvim/lua/lksh/statusline.lua
+++ nvim/lua/lksh/statusline.lua
@@ -1,5 +1,12 @@
 local statusline = {
-
+	" %t",
+	"%r",
+	"%m",
+	"%=",
+	"%{&filetype}",
+	" %2p%%",
+	" %3l:%-2c ",
 }
 
+vim.o.statusline = nil
 vim.o.statusline = table.concat(statusline, "")
