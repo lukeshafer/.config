0f75541c35a6aa7864984fa4c8a1d83cc8db0f20
Luke Shafer <shafer.361@osu.edu>
2025-04-01 14:25:10 -0400

    Remove commented plugins

--- nvim/lua/lksh/plugins.lua
+++ nvim/lua/lksh/plugins.lua
@@ -31,14 +31,7 @@ local plugins = {
 	-- { "bluz71/vim-moonfly-colors" },
 	-- { "mellow-theme/mellow.nvim" },
 	-- { "slugbyte/lackluster.nvim" },
-	-- {
-	-- 	"rose-pine/neovim",
-	-- 	name = "rose-pine",
-	-- 	lazy = false,
-	-- 	config = function()
-	-- 		vim.cmd("color rose-pine-main")
-	-- 	end,
-	-- },
+	-- { "rose-pine/neovim", name = "rose-pine" },
 
 	------------------
 	--     MINI     --
@@ -48,6 +41,19 @@ local plugins = {
 		version = false,
 		config = function()
 			vim.cmd("colorscheme randomhue")
+
+			require("mini.files").setup()
+			vim.keymap.set("n", "<leader>e", MiniFiles.open, {})
+
+			local starter = require("mini.starter")
+			starter.setup({
+				items = {
+					starter.sections.builtin_actions(),
+					{ name = "Explore Files", action = MiniFiles.open, section = "Files" },
+					starter.sections.recent_files(10, true),
+					-- starter.sections.pick(),
+				},
+			})
 
 			require("mini.icons").setup()
 			require("mini.statusline").setup()
@@ -88,9 +94,6 @@ local plugins = {
 					hex_color = hi.gen_highlighter.hex_color(),
 				},
 			})
-
-			require("mini.files").setup()
-			vim.keymap.set("n", "<leader>e", MiniFiles.open, {})
 		end,
 	},
 
@@ -134,11 +137,6 @@ local plugins = {
 		cmd = { "LspInfo", "LspInstall", "LspUninstall" },
 		config = function()
 			local lspconfig = require("lspconfig")
-			local lsp_defaults = lspconfig.util.default_config
-
-			-- lsp_defaults.capabilities =
-			-- 	vim.tbl_deep_extend("force", lsp_defaults.capabilities, require("cmp_nvim_lsp").default_capabilities())
-
 			local onWSL = os.getenv("PC_CONTEXT") == "desktop-wsl"
 
 			if onWSL then
@@ -283,50 +281,6 @@ local plugins = {
 		},
 	},
 
-	-- Autocompletion
-	-- {
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	event = "InsertEnter",
-	-- 	dependencies = {
-	-- 		"hrsh7th/cmp-nvim-lsp",
-	-- 		"hrsh7th/cmp-buffer",
-	-- 		"hrsh7th/cmp-path",
-	-- 	},
-	-- 	config = function()
-	-- 		local cmp = require("cmp")
-	--
-	-- 		cmp.setup({
-	-- 			sources = {
-	-- 				{ name = "path" },
-	-- 				{ name = "nvim_lsp", keyword_length = 0 },
-	-- 				{ name = "luasnip", keyword_length = 1 },
-	-- 				{ name = "buffer", keyword_length = 3 },
-	-- 			},
-	-- 			mapping = cmp.mapping.preset.insert({
-	-- 				["<CR>"] = cmp.mapping.confirm({ select = false }), -- Enter key confirms completion item
-	-- 				["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Ctrl + y confirms completion item
-	-- 				["<C-Space>"] = cmp.mapping.complete(), -- Ctrl + space triggers completion menu
-	-- 				["<Tab>"] = cmp.mapping.select_next_item(), -- Tab and S-Tab move through completion menu
-	-- 				["<S-Tab>"] = cmp.mapping.select_prev_item(),
-	-- 			}),
-	-- 			snippet = {
-	-- 				expand = function(args)
-	-- 					require("luasnip").lsp_expand(args.body)
-	-- 				end,
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
-	-- { "hrsh7th/cmp-nvim-lsp" },
-	-- { "hrsh7th/cmp-buffer" },
-	-- { "hrsh7th/cmp-path" },
-	-- { "saadparwaiz1/cmp_luasnip" },
-	-- { "hrsh7th/cmp-nvim-lua" },
-
-	-- Snippets
-	-- { "L3MON4D3/LuaSnip" },
-	-- { "rafamadriz/friendly-snippets" },
-
 	------------------
 	--    OTHER     --
 	------------------
@@ -363,62 +317,7 @@ local plugins = {
 			})
 		end,
 	},
-	-- { "nvim-tree/nvim-web-devicons", lazy = true },
-	-- {
-	-- 	"nvim-lualine/lualine.nvim",
-	-- 	dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-	-- 	opts = {
-	-- 		sections = {
-	-- 			lualine_a = {
-	-- 				{
-	-- 					"mode",
-	-- 					fmt = function(str)
-	-- 						if str == "NORMAL" then
-	-- 							return ""
-	-- 						elseif str == "INSERT" then
-	-- 							return ""
-	-- 						elseif str == "VISUAL" or str == "V-LINE" or str == "V-BLOCK" then
-	-- 							return "󰗧"
-	-- 						elseif str == "COMMAND" then
-	-- 							return ""
-	-- 						else
-	-- 							return str
-	-- 						end
-	-- 					end,
-	-- 				},
-	-- 			},
-	-- 			lualine_b = { "branch", "diff", "diagnostics" },
-	-- 			lualine_c = {
-	-- 				{
-	-- 					"filename",
-	-- 					symbols = { modified = "●", readonly = "[READONLY]" },
-	-- 					path = 4,
-	-- 				},
-	-- 			},
-	-- 			lualine_x = { "encoding", "filetype" },
-	-- 			lualine_y = {},
-	-- 			lualine_z = { "location" },
-	-- 		},
-	-- 		inactive_sections = {
-	-- 			lualine_a = {},
-	-- 			lualine_b = {},
-	-- 			lualine_c = {
-	-- 				{
-	-- 					"filename",
-	-- 					symbols = { modified = "●", readonly = "[READONLY]" },
-	-- 					path = 4,
-	-- 				},
-	-- 			},
-	-- 			lualine_x = {},
-	-- 			lualine_y = {},
-	-- 			lualine_z = {},
-	-- 		},
-	-- 		extensions = { "nvim-tree", "toggleterm", "man" },
-	-- 	},
-	-- }, -- status line, bottom
-	-- { "lewis6991/gitsigns.nvim", opts = {} }, -- inline git helpers
-	-- { "folke/todo-comments.nvim", dependencies = "nvim-lua/plenary.nvim", opts = {} }, -- highlight todo comments, e.g. TODO:
-	-- { "windwp/nvim-autopairs", event = "InsertEnter", opts = {} }, -- auto-create brackets/parentheses
+
 	{
 		"windwp/nvim-ts-autotag", -- autoclose/rename html tags
 		event = "InsertEnter",
@@ -438,13 +337,6 @@ local plugins = {
 			{ "<leader>vd", "<cmd>DiffviewOpen origin/dev<cr>" },
 		},
 	},
-	-- {
-	-- 	"kylechui/nvim-surround", -- tools for surrounding text
-	-- 	version = "*",
-	-- 	event = "VeryLazy",
-	-- 	opts = {},
-	-- },
-	-- { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }, -- show indentation levels
 
 	{
 		"akinsho/toggleterm.nvim",
@@ -462,254 +354,6 @@ local plugins = {
 			require("toggleterm").setup({ direction = "horizontal" })
 		end,
 	}, -- Terminal pane/tab/window handler
-
-	-- {
-	-- 	"nvim-telescope/telescope.nvim", -- fuzzy finder
-	-- 	lazy = true,
-	-- 	config = {
-	-- 		defaults = { file_ignore_patterns = { "node_modules", "dist", "build", ".git" } },
-	-- 		pickers = { find_files = { hidden = true } },
-	-- 	},
-	-- 	keys = {
-	-- 		{
-	-- 			"ff",
-	-- 			function()
-	-- 				require("telescope.builtin").find_files()
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"fg",
-	-- 			function()
-	-- 				require("telescope.builtin").live_grep()
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"fb",
-	-- 			function()
-	-- 				require("telescope.builtin").buffers()
-	-- 			end,
-	-- 		},
-	-- 		{
-	-- 			"fh",
-	-- 			function()
-	-- 				require("telescope.builtin").help_tags()
-	-- 			end,
-	-- 		},
-	-- 	},
-	-- },
-
-	-- {
-	-- 	"nvim-neo-tree/neo-tree.nvim",
-	-- 	-- event = "BufEnter",
-	-- 	branch = "v3.x",
-	-- 	dependencies = {
-	-- 		"nvim-lua/plenary.nvim",
-	-- 		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-	-- 		"MunifTanjim/nui.nvim",
-	-- 	},
-	-- 	cmd = { "Neotree" },
-	-- 	keys = {
-	-- 		{
-	-- 			"<leader>e",
-	-- 			function()
-	-- 				vim.cmd("Neotree action=focus source=filesystem position=left reveal=true")
-	-- 			end,
-	-- 			noremap = true,
-	-- 		},
-	-- 		{
-	-- 			"<leader>b",
-	-- 			function()
-	-- 				vim.cmd("Neotree action=focus source=buffers position=left reveal=true")
-	-- 			end,
-	-- 			noremap = true,
-	-- 		},
-	-- 		{
-	-- 			"<leader>g",
-	-- 			function()
-	-- 				vim.cmd("Neotree action=focus source=git_status position=left reveal=true")
-	-- 			end,
-	-- 			noremap = true,
-	-- 		},
-	-- 	},
-	-- 	config = function()
-	-- 		require("neo-tree").setup({
-	-- 			-- use_default_mappings = false,
-	-- 			close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
-	-- 			enable_git_status = true,
-	-- 			enable_diagnostics = true,
-	-- 			sort_case_insensitive = true, -- used when sorting files and directories in the tree
-	-- 			window = {
-	-- 				position = "left",
-	-- 				width = 40,
-	-- 				mapping_options = {
-	-- 					noremap = true,
-	-- 					nowait = true,
-	-- 				},
-	-- 				mappings = {
-	-- 					["<space>"] = {
-	-- 						"toggle_node",
-	-- 						nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
-	-- 					},
-	-- 					["<2-LeftMouse>"] = "open",
-	-- 					["<cr>"] = "open",
-	-- 					["<esc>"] = "cancel", -- close preview or floating neo-tree window
-	-- 					["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = true } },
-	-- 					["l"] = "focus_preview",
-	-- 					["S"] = "open_split",
-	-- 					["s"] = "open_vsplit",
-	-- 					["t"] = "open_tabnew",
-	-- 					["w"] = "open_with_window_picker",
-	-- 					["C"] = "close_node",
-	-- 					["z"] = "close_all_nodes",
-	-- 					["a"] = {
-	-- 						"add",
-	-- 						config = {
-	-- 							show_path = "none", -- "none", "relative", "absolute"
-	-- 						},
-	-- 					},
-	-- 					["A"] = "add_directory", -- also accepts the optional config.show_path option like "add". this also supports BASH style brace expansion.
-	-- 					["d"] = "delete",
-	-- 					["r"] = "rename",
-	-- 					["y"] = "copy_to_clipboard",
-	-- 					["x"] = "cut_to_clipboard",
-	-- 					["p"] = "paste_from_clipboard",
-	-- 					["c"] = "copy", -- takes text input for destination, also accepts the optional config.show_path option like "add":
-	-- 					["m"] = "move", -- takes text input for destination, also accepts the optional config.show_path option like "add".
-	-- 					["Q"] = "close_window",
-	-- 					["q"] = "none",
-	-- 					["R"] = "refresh",
-	-- 					["?"] = "show_help",
-	-- 					["<"] = "prev_source",
-	-- 					[">"] = "next_source",
-	-- 					["i"] = "show_file_details",
-	-- 					["o"] = "open",
-	-- 					["oc"] = "none",
-	-- 					["od"] = "none",
-	-- 					["og"] = "none",
-	-- 					["om"] = "none",
-	-- 					["on"] = "none",
-	-- 					["os"] = "none",
-	-- 					["ot"] = "none",
-	-- 				},
-	-- 			},
-	-- 			nesting_rules = {},
-	-- 			filesystem = {
-	-- 				filtered_items = {
-	-- 					visible = false, -- when true, they will just be displayed differently than normal items
-	-- 					hide_dotfiles = false,
-	-- 					hide_gitignored = true,
-	-- 					hide_hidden = true, -- only works on Windows for hidden files/directories
-	-- 				},
-	-- 				window = {
-	-- 					mappings = {
-	-- 						["<bs>"] = "navigate_up",
-	-- 						["."] = "set_root",
-	-- 						["H"] = "toggle_hidden",
-	-- 						["/"] = "fuzzy_finder",
-	-- 						["D"] = "fuzzy_finder_directory",
-	-- 						["#"] = "fuzzy_sorter", -- fuzzy sorting using the fzy algorithm
-	-- 						["f"] = "filter_on_submit",
-	-- 						["<c-x>"] = "clear_filter",
-	-- 						["[g"] = "prev_git_modified",
-	-- 						["]g"] = "next_git_modified",
-	-- 						["o"] = "open",
-	-- 						["oc"] = "none",
-	-- 						["od"] = "none",
-	-- 						["og"] = "none",
-	-- 						["om"] = "none",
-	-- 						["on"] = "none",
-	-- 						["os"] = "none",
-	-- 						["ot"] = "none",
-	-- 					},
-	-- 					fuzzy_finder_mappings = { -- define keymaps for filter popup window in fuzzy_finder_mode
-	-- 						["<down>"] = "move_cursor_down",
-	-- 						["<C-n>"] = "move_cursor_down",
-	-- 						["<up>"] = "move_cursor_up",
-	-- 						["<C-p>"] = "move_cursor_up",
-	-- 						-- ['<key>'] = function(state, scroll_padding) ... end,
-	-- 					},
-	-- 				},
-	--
-	-- 				commands = {}, -- Add a custom command or override a global one using the same function name
-	-- 			},
-	-- 			buffers = {
-	-- 				follow_current_file = {
-	-- 					enabled = true, -- This will find and focus the file in the active buffer every time
-	-- 					--              -- the current file is changed while the tree is open.
-	-- 					leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
-	-- 				},
-	-- 				group_empty_dirs = true, -- when true, empty folders will be grouped together
-	-- 				show_unloaded = true,
-	-- 				window = {
-	-- 					mappings = {
-	-- 						["d"] = "buffer_delete",
-	-- 						["bd"] = "buffer_delete",
-	-- 						["<bs>"] = "navigate_up",
-	-- 						["."] = "set_root",
-	-- 						["o"] = "open",
-	-- 						["oc"] = "none",
-	-- 						["od"] = "none",
-	-- 						["og"] = "none",
-	-- 						["om"] = "none",
-	-- 						["on"] = "none",
-	-- 						["os"] = "none",
-	-- 						["ot"] = "none",
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 			git_status = {
-	-- 				window = {
-	-- 					position = "float",
-	-- 					mappings = {
-	-- 						["A"] = "git_add_all",
-	-- 						["gu"] = "git_unstage_file",
-	-- 						["ga"] = "git_add_file",
-	-- 						["gr"] = "git_revert_file",
-	-- 						["gc"] = "git_commit",
-	-- 						["gp"] = "git_push",
-	-- 						["gg"] = "git_commit_and_push",
-	-- 						["o"] = "open",
-	-- 						["oc"] = "none",
-	-- 						["od"] = "none",
-	-- 						["og"] = "none",
-	-- 						["om"] = "none",
-	-- 						["on"] = "none",
-	-- 						["os"] = "none",
-	-- 						["ot"] = "none",
-	-- 					},
-	-- 				},
-	-- 			},
-	-- 		})
-	--
-	-- 		-- vim.cmd("Neotree show")
-	-- 	end,
-	-- },
-
-	-- { "prichrd/netrw.nvim", opts = {
-	-- 	config = function()
-	-- 		require("netrw").configure({})
-	-- 	end,
-	-- } },
-
-	-- highlights matches to the word under the cursor
-	-- {
-	-- 	"RRethy/vim-illuminate",
-	-- 	event = "VeryLazy",
-	-- 	config = function()
-	-- 		require("illuminate").configure({})
-	-- 	end,
-	-- },
-
-	-- {
-	-- 	"brenoprata10/nvim-highlight-colors", -- highlight hex color strings e.g. #CCCCFF, text-sky-700
-	-- 	config = function()
-	-- 		require("nvim-highlight-colors").setup({
-	-- 			render = "background", -- 'foreground' or 'background' or 'virtual'
-	-- 			enable_named_colors = false,
-	-- 			enable_tailwind = true, -- bg-blue-450
-	-- 		})
-	-- 	end,
-	-- },
 
 	{
 		"uga-rosa/ccc.nvim", -- color picker
@@ -743,25 +387,6 @@ local plugins = {
 			{ "<leader>cs", "<cmd>CsvViewToggle<cr>" },
 		},
 	},
-	-- {
-	-- 	"cameron-wags/rainbow_csv.nvim",
-	-- 	config = true,
-	-- 	ft = {
-	-- 		"csv",
-	-- 		"tsv",
-	-- 		"csv_semicolon",
-	-- 		"csv_whitespace",
-	-- 		"csv_pipe",
-	-- 		"rfc_csv",
-	-- 		"rfc_semicolon",
-	-- 	},
-	-- 	cmd = {
-	-- 		"RainbowDelim",
-	-- 		"RainbowDelimSimple",
-	-- 		"RainbowDelimQuoted",
-	-- 		"RainbowMultiDelim",
-	-- 	},
-	-- },
 }
 
 require("lazy").setup(plugins)
