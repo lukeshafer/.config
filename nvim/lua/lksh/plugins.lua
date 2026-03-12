26a1e1af47f812643ce2f0b5c621e1486f8b2191
Luke Shafer <shafer.361@osu.edu>
2026-03-12 12:18:05 -0400

    Telescope lazy loading

--- nvim/lua/lksh/plugins.lua
+++ nvim/lua/lksh/plugins.lua
@@ -234,6 +234,7 @@ local plugins = {
 			defaults = { file_ignore_patterns = { "node_modules", "dist", "build", ".git" } },
 			pickers = { find_files = { hidden = true } },
 		},
+    cmd = { "Telescope" },
 		keys = {
 			{
 				"ff",
