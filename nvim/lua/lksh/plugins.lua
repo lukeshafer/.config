bb6a9725f071660aaf7642eaf8c54620950bf81d
Luke Shafer <shafer.361@osu.edu>
2026-04-03 11:40:37 -0400

    nvim plugin management

--- nvim/lua/lksh/plugins.lua
+++ nvim/lua/lksh/plugins.lua
@@ -1,188 +1,62 @@
-local utils = require("lksh.utils")
+---@class PluginDef
+---@field src string
+---@field deps? string[]
 
-vim.pack.add({
-	"https://github.com/nvim-lua/plenary.nvim",
-	"https://github.com/nvim-tree/nvim-web-devicons",
-	"https://github.com/hrsh7th/cmp-nvim-lsp",
-	"https://github.com/hrsh7th/cmp-buffer",
-	"https://github.com/hrsh7th/cmp-path",
-	"https://github.com/saadparwaiz1/cmp_luasnip",
-	"https://github.com/hrsh7th/cmp-nvim-lua",
-	"https://github.com/hrsh7th/nvim-cmp",
-	"https://github.com/echasnovski/mini.colors",
-	"https://github.com/echasnovski/mini.hues",
-	"https://github.com/stevearc/conform.nvim", -- Formatter management
-	"https://github.com/williamboman/mason.nvim",
-	"https://github.com/neovim/nvim-lspconfig",
-	"https://github.com/L3MON4D3/LuaSnip",
-	"https://github.com/rafamadriz/friendly-snippets",
-	"https://github.com/nvim-treesitter/nvim-treesitter",
-	"https://github.com/lewis6991/gitsigns.nvim",
-	"https://github.com/folke/todo-comments.nvim",
-	"https://github.com/windwp/nvim-autopairs",
-	"https://github.com/windwp/nvim-ts-autotag",
-	"https://github.com/sindrets/diffview.nvim",
-	"https://github.com/kylechui/nvim-surround",
-	"https://github.com/lukas-reineke/indent-blankline.nvim",
-	"https://github.com/akinsho/toggleterm.nvim",
-	"https://github.com/nvim-telescope/telescope.nvim", -- fuzzy finder
-	"https://github.com/MunifTanjim/nui.nvim",
-	"https://github.com/nvim-neo-tree/neo-tree.nvim",
-	"https://github.com/RRethy/vim-illuminate",
-	"https://github.com/brenoprata10/nvim-highlight-colors", -- highlight hex/tailwind color strings e.g. #CCCCFF, text-sky-700
+---@param url string
+local function resolve(url)
+	return url:find("://") and url or "https://github.com/" .. url
+end
+
+---Light wrapper around vim.pack.add to mark dependencies
+---@param plugins (PluginDef|string)[]
+local function load_plugins(plugins)
+	local list = {}
+	for _, p in ipairs(plugins) do
+		if type(p) == "string" then
+			list[#list + 1] = resolve(p)
+		else
+			for _, dep in ipairs(p.deps or {}) do
+				list[#list + 1] = resolve(dep)
+			end
+			list[#list + 1] = resolve(p.src)
+		end
+	end
+	vim.pack.add(list)
+end
+
+load_plugins({
+	"nvim-lua/plenary.nvim",
+	"nvim-tree/nvim-web-devicons",
+	{
+		src = "hrsh7th/nvim-cmp",
+		deps = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+			"L3MON4D3/LuaSnip",
+			"hrsh7th/cmp-nvim-lua",
+		},
+	},
+	{ src = "nvim-mini/mini.hues", deps = { "nvim-mini/mini.colors" } },
+	"stevearc/conform.nvim", -- Formatter management
+	"williamboman/mason.nvim",
+	"neovim/nvim-lspconfig",
+	"rafamadriz/friendly-snippets",
+	"nvim-treesitter/nvim-treesitter",
+	"lewis6991/gitsigns.nvim",
+	"folke/todo-comments.nvim",
+	"windwp/nvim-autopairs",
+	"windwp/nvim-ts-autotag",
+	"sindrets/diffview.nvim",
+	"kylechui/nvim-surround",
+	"lukas-reineke/indent-blankline.nvim",
+	"akinsho/toggleterm.nvim",
+	"nvim-telescope/telescope.nvim", -- fuzzy finder
+	"MunifTanjim/nui.nvim",
+	"nvim-neo-tree/neo-tree.nvim",
+	"RRethy/vim-illuminate",
+	"brenoprata10/nvim-highlight-colors", -- highlight hex/tailwind color strings e.g. #CCCCFF, text-sky-700
 	-- "https://github.com/uga-rosa/ccc.nvim", -- color picker
 	-- "https://github.com/hat0uma/csvview.nvim",
 })
-
-require("mason").setup({})
-require("gitsigns").setup({})
-require("todo-comments").setup({})
-require("nvim-autopairs").setup({})
-require("nvim-autopairs").setup({})
-require("nvim-ts-autotag").setup({})
-require("nvim-surround").setup({})
-require("illuminate").configure({})
-
-local cmp = require("cmp")
-
-cmp.setup({
-	sources = cmp.config.sources({
-		{ name = "nvim_lsp", keyword_length = 0 },
-		{ name = "luasnip", keyword_length = 1 },
-	}, {
-		{ name = "path" },
-		{ name = "buffer", keyword_length = 3 },
-	}),
-	mapping = cmp.mapping.preset.insert({
-		["<CR>"] = cmp.mapping.confirm({ select = false }), -- Enter key confirms completion item
-		["<C-y>"] = cmp.mapping.confirm({ select = true }), -- Ctrl + y confirms completion item
-		["<C-Space>"] = cmp.mapping.complete(), -- Ctrl + space triggers completion menu
-		["<Tab>"] = cmp.mapping.select_next_item(), -- Tab and S-Tab move through completion menu
-		["<S-Tab>"] = cmp.mapping.select_prev_item(),
-	}),
-	snippet = {
-		expand = function(args)
-			vim.snippet.expand(args.body)
-		end,
-	},
-})
-
-local conform = require("conform")
-
-conform.setup({
-	formatters_by_ft = {
-		lua = { "stylua" },
-		javascript = utils.js_formatter,
-		typescript = utils.js_formatter,
-		javascriptreact = utils.js_formatter,
-		typescriptreact = utils.js_formatter,
-		jsx = utils.js_formatter,
-		tsx = utils.js_formatter,
-		astro = utils.js_formatter,
-		json = { "fixjson" },
-		java = { "google-java-format" },
-		go = { "golines", "goimports", "gofumpt" },
-		soql = { "sleek" },
-	},
-})
-
-vim.keymap.set("n", "<leader>f", function()
-	conform.format({ timeout_ms = 2000, lsp_fallback = true })
-end, { noremap = true, silent = true })
-
-vim.keymap.set("n", "<leader>vo", "<cmd>DiffviewOpen<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>vc", "<cmd>DiffviewClose<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>vm", "<cmd>DiffviewOpen origin/main<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>vd", "<cmd>DiffviewOpen origin/dev<cr>", { noremap = true, silent = true })
-
-local seed = utils.get_seed_from_string(vim.fn.getcwd())
-require("lksh.colors").prepare_color_theme(seed)
-
-require("neo-tree").setup({
-	sort_case_insensitive = true,
-	window = {
-		position = "float",
-		mappings = {
-			["o"] = "open",
-			["oc"] = "none",
-			["od"] = "none",
-			["og"] = "none",
-			["om"] = "none",
-			["on"] = "none",
-			["os"] = "none",
-			["ot"] = "none",
-		},
-	},
-	filesystem = {
-		filtered_items = {
-			visible = false, -- when true, they will just be displayed differently than normal items
-			hide_dotfiles = false,
-			hide_gitignored = true,
-			hide_hidden = true, -- only works on Windows for hidden files/directories
-		},
-	},
-})
-
-vim.keymap.set("n", "<leader>e", "<cmd>Neotree reveal<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>b", "<cmd>Neotree buffers reveal<cr>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>g", "<cmd>Neotree git_status reveal<cr>", { noremap = true, silent = true })
-
-require("nvim-highlight-colors").setup({
-	render = "background", -- 'foreground' or 'background' or 'virtual'
-	enable_named_colors = false,
-	enable_tailwind = true, -- bg-blue-450
-})
-
-local nvim_treesitter = require("nvim-treesitter")
-nvim_treesitter.install({
-	"css",
-	"typescript",
-	"tsx",
-	"astro",
-	"lua",
-	"html",
-	"gitignore",
-	"http",
-	"jsdoc",
-	"javascript",
-	"json",
-	"json5",
-	"markdown",
-	"sql",
-	"toml",
-	"yaml",
-	"bash",
-	"comment",
-})
-
-vim.api.nvim_create_autocmd("PackChanged", {
-	callback = function()
-		nvim_treesitter.update()
-	end,
-})
-
-require("telescope").setup({
-	defaults = { file_ignore_patterns = { "node_modules", "dist", "build" } },
-	pickers = { find_files = { hidden = true } },
-})
-
-vim.keymap.set("n", "ff", function()
-	require("telescope.builtin").find_files()
-end, { noremap = true, silent = true })
-vim.keymap.set("n", "fg", function()
-	require("telescope.builtin").live_grep()
-end, { noremap = true, silent = true })
-vim.keymap.set("n", "fb", function()
-	require("telescope.builtin").buffers()
-end, { noremap = true, silent = true })
-vim.keymap.set("n", "fh", function()
-	require("telescope.builtin").help_tags()
-end, { noremap = true, silent = true })
-
-require("toggleterm").setup({ direction = "horizontal" })
-vim.keymap.set("n", "<leader>t", "<cmd>ToggleTerm<cr>", { noremap = true, silent = true })
-vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], { noremap = true, silent = true })
-vim.keymap.set("t", "<C-h>", "<cmd>wincmd h<cr>", { noremap = true, silent = true })
-vim.keymap.set("t", "<C-j>", "<cmd>wincmd j<cr>", { noremap = true, silent = true })
-vim.keymap.set("t", "<C-k>", "<cmd>wincmd k<cr>", { noremap = true, silent = true })
-vim.keymap.set("t", "<C-l>", "<cmd>wincmd l<cr>", { noremap = true, silent = true })
