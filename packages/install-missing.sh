706b03f75a3f945dcbe0545c25cf94fa8f4b2816
Luke Shafer <lukealanshafer@gmail.com>
2026-07-25 22:25:16 -0400

    package file updates

---
+++ packages/install-missing.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+yay -S 7zip clipman file-roller grim intel-ucode libreoffice-fresh slurp sof-firmware swayosd trash-cli ttf-nerd-fonts-symbols ttf-nerd-fonts-symbols-mono ttf-noto-nerd ttf-victor-mono-nerd ttf-zed-mono-nerd wf-recorder wl-clipboard
