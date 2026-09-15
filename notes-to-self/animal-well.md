c5c29a7d3ca1827c6dd49e7eefa7d4bea41b06f9
Luke Shafer <shafer.361@osu.edu>
2026-09-15 15:36:08 -0400

    stuff

---
+++ notes-to-self/animal-well.md
@@ -0,0 +1,5 @@
+My general thoughts on Animal Well, FULL spoilers! If you are still playing the game at all and don't want to be spoiled, do not click
+
+||Context: I've collected all the eggs and entered the mouth of the second manticore||
+
+The first "layer" (i.e. collecting the 4 flames, up to seeing the _first_ credits) was definitely my favorite part of the game. I felt like the puzzles were really fun 
--- nvim/after/plugin/zen-mode.lua
+++ nvim/after/plugin/zen-mode.lua
@@ -11,7 +11,7 @@ vim.keymap.set("n", "<leader>z", function()
 			-- * an absolute number of cells when > 1
 			-- * a percentage of the width / height of the editor when <= 1
 			-- * a function that returns the width or the height
-			width = 80, -- width of the Zen window
+			width = 100, -- width of the Zen window
 			height = 1, -- height of the Zen window
 			-- by default, no options are changed for the Zen window
 			-- uncomment any of the options below, or add other vim.wo options you want to apply
@@ -19,7 +19,8 @@ vim.keymap.set("n", "<leader>z", function()
 				signcolumn = "no", -- disable signcolumn
 				number = false, -- disable number column
 				relativenumber = false, -- disable relative numbers
-				-- cursorline = false, -- disable cursorline
+				cursorline = false, -- disable cursorline
+				guicursor = "n-v-c:hor25",
 				-- cursorcolumn = false, -- disable cursor column
 				foldcolumn = "0", -- disable fold column
 				-- list = false, -- disable whitespace characters
